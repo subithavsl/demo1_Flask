5a66f2bb6fb8f35b80394c3a0919f8a8f1511cad
subithavsl <subithar.cse2020@dscet.ac.in>
2024-04-23 12:24:10 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# this is python configuration file
+from flask import Flask  # this is a flask package
+
+app=Flask(__name__)
+
+@app.route('/')# default api view function
+def home():
+  return "this is home"
+
+@app.route('/s2s/api/signup')
+def user_signup():
+  return "this is a user signup page"
+  
+
+if __name__ =="__main__":
+  app.run(debug= True)
